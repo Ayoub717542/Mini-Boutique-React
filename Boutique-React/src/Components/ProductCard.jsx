5d563fcd38a245ee0e70a3b9898664c02bbe7a66
Ayoub Hadi <AyoubHadi856@gmail.com>
2026-07-08 09:43:56 +0100

    nothing change

--- Boutique-React/src/Components/ProductCard.jsx
+++ Boutique-React/src/Components/ProductCard.jsx
@@ -1,35 +1,10 @@
-import { use, useState } from "react";
-
 function ProductCard({ product, onDeleteProduct, onAddToCart }) {
-  const [showConfirm, setShowConfirm] = useState(false);
   return (
     <div className="card_content">
       <i
         className="x-close fa fa-times-circle"
-        onClick={() => setShowConfirm(true)}
+        onClick={() => onDeleteProduct(product.id)}
       ></i>
-      { showConfirm  && (
- <div>
-  <label>
-    Yes
-    <input 
-      type="radio" 
-      name="product-choice" 
-      value="Yes" 
-      onClick={() => onDeleteProduct(product.id)} 
-    />
-  </label>  
-  <label>
-    No
-    <input 
-      type="radio" 
-      name="product-choice" 
-      value="NO" 
-      onClick={()=> setShowConfirm(false)}
-    />
-  </label>
-</div>
-      )}
       <img src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
       <p>{product.type} </p>
