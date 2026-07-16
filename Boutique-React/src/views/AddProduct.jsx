07bfb9dde93d66f5d56b143cdef39ee83727cc76
Ayoub Hadi <AyoubHadi856@gmail.com>
2026-07-16 16:58:59 +0100

    update addProduct

--- Boutique-React/src/views/AddProduct.jsx
+++ Boutique-React/src/views/AddProduct.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import productSchema from "../validation/productSchema";
-import { Formik,Field,ErrorMessage,Form } from "formik";
-console.log(productSchema);
+import {Formik , Field, ErrorMessage, Form } from "formik";
 
 const initialValues={
   name:"",
